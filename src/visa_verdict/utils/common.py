f061d61e7fdaf91921b76919774f119d0ad96274
gititsid <secidsid@gmail.com>
2024-09-01 14:42:49 +0530

    mongodb demo added

--- src/visa_verdict/utils/common.py
+++ src/visa_verdict/utils/common.py
@@ -1,4 +1,11 @@
+import os
 import sys
+
+import dill
+import yaml
+import numpy as np
+from pandas import DataFrame
+
 from functools import wraps
 
 from src.visa_verdict.logger import logging
@@ -44,6 +51,103 @@ def log_handler(func: callable) -> callable:
         logging.info(f"Function '{func.__name__}' executed successfully!")
         return result
     return wrapper
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def load_object(file_path: str) -> object:
+    logging.info("Entered the load_object method of utils")
+
+    try:
+
+        with open(file_path, "rb") as file_obj:
+            obj = dill.load(file_obj)
+
+        logging.info("Exited the load_object method of utils")
+
+        return obj
+
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def save_object(file_path: str, obj: object) -> None:
+    logging.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logging.info("Exited the save_object method of utils")
+
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def drop_columns(df: DataFrame, cols: list) -> DataFrame:
+    """
+    drop the columns form a pandas DataFrame
+    df: pandas DataFrame
+    cols: list of columns to be dropped
+    """
+    logging.info("Entered drop_columns method of utils")
+
+    try:
+        df = df.drop(columns=cols, axis=1)
+
+        logging.info("Exited the drop_columns method of utils")
+
+        return df
+    except Exception as e:
+        raise CustomException(e, sys) from e
 
 
 if __name__ == "__main__":
