febcdd0d2950f79024845a74a2dc950e90c26acb
gititsid <secidsid@gmail.com>
2024-09-10 22:27:45 +0530

    reading data in parallel to save time

--- src/visa_verdict/utils/monogdb_utilities.py
+++ src/visa_verdict/utils/monogdb_utilities.py
@@ -2,7 +2,10 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from concurrent.futures import ThreadPoolExecutor
+
 from src.visa_verdict.logger import logging
+from src.visa_verdict.exception import CustomException
 
 # Load environment variables
 load_dotenv()
@@ -29,11 +32,40 @@ class MongoDBUtilities:
         return self.collection.insert_one(data)
 
     def insert_many(self, data: list[dict]):
-        return self.collection.insert_many(data)
+        try:
+            logging.info(
+                f"Inserting {len(data)} records to MongoDB! DB: {self.db_name}, Collection: {self.collection_name}"
+            )
+            load_data_to_db = self.collection.insert_many(data)
+            logging.info(f"Successfully inserted {len(load_data_to_db.inserted_ids)} records to MongoDB!")
+        except Exception as e:
+            logging.error(f"Error inserting data to MongoDB! Error: {e}")
+
+    def _fetch_batch(self, skip, limit):
+        return list(self.collection.find().skip(skip).limit(limit))
 
     def read_all_as_df(self):
-        data = list(self.collection.find())
-        return pd.DataFrame(data)
+        try:
+            logging.info(
+                f"Reading all records from MongoDB! Database: {self.db_name}, Collection: {self.collection_name} ..."
+            )
+
+            # Parameters
+            batch_size: int = 1000
+            total_docs: int = self.collection.count_documents({})
+            dataframe = pd.DataFrame()
+
+            # Use ThreadPoolExecutor for parallel fetching
+            with ThreadPoolExecutor() as executor:
+                futures = [executor.submit(self._fetch_batch, i, batch_size) for i in range(0, total_docs, batch_size)]
+                for future in futures:
+                    batch = future.result()
+                    dataframe = pd.concat([dataframe, pd.DataFrame(batch)], ignore_index=True)
+
+            logging.info(f"Successfully read {len(dataframe)} records from MongoDB!")
+            return dataframe
+        except Exception as e:
+            logging.error(f"Error reading data from MongoDB! Error: {e}")
 
 
 if __name__ == "__main__":
